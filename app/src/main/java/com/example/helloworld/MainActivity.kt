dbad2cb27293956e91adde4285ca56f24f7bb83f
Ritika Suresh <ritikasuresh19@gmail.com>
2022-09-07 10:55:31 -0400

    Initial commit

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,19 +1,33 @@
 package com.example.helloworld
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button = findViewById<Button>(R.id.helloButton)
-        button.setOnClickListener {
+        val introText = findViewById<TextView>(R.id.introTextView)
+        val helloButton = findViewById<Button>(R.id.helloButton)
+        val introColorChangeButton = findViewById<Button>(R.id.introColorChangeButton)
+
+        helloButton.setOnClickListener {
             Log.v("Hello world" , "Button clicked!")
             Toast.makeText(this , "Hello to you too!" , Toast.LENGTH_SHORT).show()
+        }
+
+        introColorChangeButton.setOnClickListener {
+            val rnd = Random.Default
+            val clr = Color.argb(255 , rnd.nextInt(256) , rnd.nextInt(256) , rnd.nextInt(256))
+            Log.v("color change" , "Intro text color changed")
+            introText.setTextColor(clr)
         }
 
     }
